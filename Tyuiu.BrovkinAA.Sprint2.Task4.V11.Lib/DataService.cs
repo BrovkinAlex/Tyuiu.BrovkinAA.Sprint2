92e190f3c0e6adf8cf43a0042ea316e09c646f59
BrovkinAlex <Screep_dogg@mail.ru>
2024-09-27 18:11:01 +0300

    Task4

--- Tyuiu.BrovkinAA.Sprint2.Task4.V11.Lib/DataService.cs
+++ Tyuiu.BrovkinAA.Sprint2.Task4.V11.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.BrovkinAA.Sprint2.Task4.V11.Lib
         public double Calculate(double x, double y)
         {
             double res = (x - 20 * 2) < y / 4 ? Math.Pow((3 + (8 / Math.Pow(x, 2))), y) : -Math.Pow((x + 1) / (y + 2), x);
-            return res;
+            return Math.Round(res, 3);
         }
     }
 }
